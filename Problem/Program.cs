c61d21cc0e9ed2f42881e2120a5533cdd7a4e5c3
Sergio Ricardo Hideki Nisikava <sergionisikava@gmail.com>
2024-06-06 21:57:53 -0300

    scaffold

---
+++ Problem/Program.cs
@@ -0,0 +1,18 @@
+﻿class Solution
+{
+    public string Method(string input)
+    {
+        return "";
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Solution solution = new Solution();
+        string input = "input";
+        string result = solution.Method(input);
+        Console.WriteLine("result");
+    }
+}
