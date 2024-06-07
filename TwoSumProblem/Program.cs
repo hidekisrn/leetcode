74aab4aba374416e826646ed7f86781a0e8b234a
Sergio Ricardo Hideki Nisikava <sergionisikava@gmail.com>
2024-06-06 21:49:37 -0300

    twosum

---
+++ TwoSumProblem/Program.cs
@@ -0,0 +1,34 @@
+﻿class Solution
+{
+    public int[] TwoSum(int[] nums, int target)
+    {
+        Dictionary<int, int> map = new();
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            int complement = target - nums[i];
+            if (map.ContainsKey(complement))
+            {
+                return new int[] { map[complement], i };
+            }
+            if (!map.ContainsKey(nums[i]))
+            {
+                map.Add(nums[i], i);
+            }
+        }
+
+        throw new ArgumentException("No two sum solution");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Solution solution = new();
+        int[] nums = { 2, 7, 11, 15 };
+        int target = 9;
+        int[] result = solution.TwoSum(nums, target);
+        Console.WriteLine($"{result[0]} {result[1]}");
+    }
+}
